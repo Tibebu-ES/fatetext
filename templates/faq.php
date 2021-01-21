c6ecc1a9989a966f25ca073dc6811eb32e27a5fd
Conr Stonewall <conr@Conrs-MacBook-Pro.local>
2021-01-21 11:49:10 -0500

    queuing faqreport

--- templates/faq.php
+++ templates/faq.php
@@ -4,47 +4,7 @@
 
 <p>At 12noon, <a href="http://tsuzy.com">TSUZY</a> expects to publish a video that demonstrates and showcases the development of the following features of the FATE software, as originally described by the printed, summer 2020 version of <a href="http://thesuzy.com">TheSuzy.com</a> Show and _______ in 2015 online:</p>
 
-<p><b>___M. DataTextViz___</b></p>
-
-Separate working prototypes of the following idea have been known to exist in the years 2015 and 2020, respectively:
-
-<br><br>
-
-<i>"A Monte-Carlo Method for Elaborating the Meaning of a Text"</i>
-
-<br><br>
-
-<u>Without loss of generality, let</u>:
-
-<br><br>
-
-SENTENCE: "And the LORD God formed man of the dust of the ground"<br>
-WORD: "formed"<br>
-GEM: "And the LORD God _______ man of the dust of the ground"<br>
-TEXT: the Bible
-
-<br><br>
-
-ORACULAR: select a random SENTENCE and redact a WORD in order to produce a GEM<br>
-SYSTEM: FATEtext (implemented in PHP), textORACLE (python), etc.<br>
-USER: you (the reader of this human readable dataset)
-
-<br><br>
-
-Step1: ask the USER of a software SYSTEM to guess what WORD was redacted <b>-or</b><br>
-STEP1: ask the USER of a software SYSTEM to define the WORD, without any context
-
-<br><br>
-
-Step2: ask the USER to ask a question about the SENTENCE after guessing the WORD <b>-or-</b><br>
-STEP2: ask the USER redefine the WORD in the context of the SENTENCE, only
-
-<br><br>
-
-Step3: ask the USER to answer their own question, in accordance with the meaning of the TEXT <b>-or-</b><br>
-STEP3: ask the USER to represent the WORD in the context of the TEXT and the world in which everything exists
-
-<br><br>
+<p><b>M. DataTextViz</b></p>
 
 ::
 
